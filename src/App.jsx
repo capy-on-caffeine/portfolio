e8bd9e62bb54c1c3f9462c7b933589d818851196
capy-on-caffeine <arnav.anand2004@gmail.com>
2024-06-29 09:45:36 +0530

    Ping script for jiffy render backend

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,28 @@ import ScrollToHashElement from "./components/utils/ScrollToHashElement";
 import Stack from "./components/Stack";
 
 export default function App() {
+  function startPingServer(url) {
+    function pingServer() {
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    console.log('Ping successful');
+                    console.log(response);
+                } else {
+                    console.error('Ping failed: ', response.status);
+                }
+            })
+            .catch(error => {
+                console.error('Ping error: ', error);
+            });
+    }
+    setInterval(pingServer, 14 * 60 * 1000);
+}
   return (
     <div>
+      {
+        startPingServer('jiffy-site-backend.onrender.com/getwaitlist')
+      }
       <ScrollToHashElement />
       <Navbar />
       <Hero />
