d83ae0b200233b0cf3f315447a806fb33c1f30f3
capy-on-caffeine <arnav.anand2004@gmail.com>
2025-07-14 19:22:55 +0530

    basic hero section text

---
+++ src/components/landingPage/HeroSection.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='w-full min-h-screen bg-[#222222] text-white text-5xl flex justify-center items-center'>
+        <div className='flex flex-col gap-10'>
+            <div className='flex flex-col gap-2'>
+                <p>
+                    hey i'm <span className='text-[#519CE6]'>arnav</span>
+                </p>
+                <p className='text-7xl'>
+                    developer and designer
+                </p>
+                <p>
+                    based in india
+                </p>
+            </div>
+
+            <div className='flex flex-col gap-2'>
+                <p>
+                    i make cool software, design pretty stuff,
+                </p>
+                <p>
+                    and <span className='text-[#519CE6]'>subvert expectations</span>
+                </p>
+            </div>
+
+            <div className='flex gap-12'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
