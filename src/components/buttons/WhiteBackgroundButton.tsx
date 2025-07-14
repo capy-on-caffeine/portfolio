f964432469f8332ee9b4be2a51fb8e568d736c50
capy-on-caffeine <arnav.anand2004@gmail.com>
2025-07-14 19:54:43 +0530

    added custom cursor

--- src/components/buttons/WhiteBackgroundButton.tsx
+++ src/components/buttons/WhiteBackgroundButton.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import React from "react";
+import { Button } from "../ui/button";
 
-const WhiteBackgroundButton: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const WhiteBackgroundButton: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
-    <Button className='bg-white text-black text-2xl px-16 py-7 rounded-full'>
-        {children}
+    <Button
+      className="bg-white text-black text-2xl px-16 py-7 rounded-full cursor"
+      style={{ cursor: "url('/images/cursor.png'), auto" }}
+    >
+      {children}
     </Button>
-  )
-}
+  );
+};
 
-export default WhiteBackgroundButton
+export default WhiteBackgroundButton;
